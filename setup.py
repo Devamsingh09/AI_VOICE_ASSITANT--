e11a39147e44bdb2d4d7ec0551d3a0991d09e88b
Devamsingh09 <141946786+Devamsingh09@users.noreply.github.com>
2025-02-18 16:06:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,18 @@
-from setuptools import find_packages, setup
-setup(name = "AI Voice Assistant", version =" 0.0.1", author = "Devam Singh", author_email = "devamsingh0009@gmail.com", packages = find_packages(__all__))
+from setuptools import find_packages
+
+setup(
+    name="AI Voice Assistant",
+    version="0.0.1",
+    author="Devam Singh",
+    author_email="devamsingh0009@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "SpeechRecognition==3.14.1",
+        "pipwin==0.5.2",
+        "pyaudio==0.2.14",
+        "gTTS==2.5.4",
+        "google-generativeai==0.8.4",
+        "python-dotenv==1.0.1",
+        "streamlit==1.42.0",
+    ],
+)
