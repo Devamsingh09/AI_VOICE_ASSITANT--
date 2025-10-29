3e79ab0391297593dd2bbd432f2fcfa5aadb3a62
Devam Singh <singhdevam68@gmail.com>
2025-10-30 01:57:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,33 @@
+import speech_recognition as sr
+from gtts import gTTS
+import google.generativeai as palm
+import os
+from dotenv import load_dotenv
 import streamlit as st
-from src.helper import llm_model, voice_input, text_to_speech
+from src.helper import voice_input, llm_model, text_to_speech
+
 
 def main():
-    st.title("AI Voice Assistant🤖 (Multilingual)")
-
-    if 'voice_text' not in st.session_state:
-        st.session_state.voice_text = None
-
-    if st.button("Start Listening"):
-        with st.spinner("Listening...👂"):
+    st.title("AI VOICE ASSISTANT (Using GEMINI )")
+    
+    if st.button("Ask me anything"):
+        with st.spinner('Listening...'):
             text = voice_input()
             if text:
-                st.session_state.voice_text = text
-                st.success("Voice input captured!")
-                st.write(f"You said: {text}")
-            else:
-                st.error("Could not understand the audio. Please try again.")
-
-    if st.session_state.voice_text:
-        if st.button("Submit"):
-            with st.spinner("Processing...🤖"):
-                response = llm_model(st.session_state.voice_text)
+                response=llm_model(text)
                 text_to_speech(response)
 
-                audio_file = open('speech.mp3', 'rb')
-                audio_bytes = audio_file.read()
-
-                st.text_area(label="Response:", value=response, height=390)
+                audio_file=open("speech.mp3","rb")
+                audio_bytes=audio_file.read()
+                st.text_area(label="Response", value=response, height=350)
                 st.audio(audio_bytes)
-                st.download_button(label="Download speech",
-                                   data=audio_bytes,
+
+                st.download_button(label="Download speech",data=audio_bytes,
                                    file_name="speech.mp3",
-                                   mime="audio/mp3")
-                # Clear the voice text after processing
-                st.session_state.voice_text = None
-
-
-
-
-
+                                   mime='audio/mp3')
+            else:
+                st.error("No speech detected or recognized. Please try again.")
+            
 if __name__=="__main__":
     main()
+            
